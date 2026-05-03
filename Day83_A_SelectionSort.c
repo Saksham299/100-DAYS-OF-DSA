052780fd5a7ee4e099ace95ce66b8e22b7f31f6b
Saksham Verma <Saksham.28288@stu.upes.ac.in>
2026-05-03 23:07:13 +0530

    Create Day83_A_SelectionSort.c

---
+++ Day83_A_SelectionSort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, arr[100];
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n-1; i++) {
+        int minIdx = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[minIdx])
+                minIdx = j;
+        }
+        int temp = arr[i];
+        arr[i] = arr[minIdx];
+        arr[minIdx] = temp;
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
